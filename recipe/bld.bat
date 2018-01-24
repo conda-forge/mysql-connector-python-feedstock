ee922338238a276161595ac6828212be45af24ec
Stuart Campbell <stuart@stuartcampbell.me>
2018-01-23 22:04:40 -0500

    Fix linter errors

--- recipe/bld.bat
+++
@@ -1,6 +0,0 @@
-"%PYTHON%" setup.py install --with-protobuf-include-dir=$LIBRARY_INC  
-                            --with-protobuf-lib-dir=$LIBRARY_LIB 
-                            --with-protoc=$LIBRARY_BIN/protoc.exe
-			    --single-version-externally-managed 
-                            --record=record.txt
-if errorlevel 1 exit 1
