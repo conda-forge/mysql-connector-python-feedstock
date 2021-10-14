385649add68f40d55a074c2d4708a8df5e42d452
Nehal J Wani <nehaljw.kkd1@gmail.com>
2021-10-14 11:23:54 -0400

    Update build_steps.sh

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -27,6 +27,10 @@ conda-build:
 CONDARC
 BUILD_CMD=build
 
+conda list
+
+conda update ca-certificates -y -c conda-forge
+
 curl -vvv -sLO https://github.com/mysql/mysql-connector-python/archive/8.0.25.tar.gz
 
 conda install --yes --quiet "conda-forge-ci-setup=3" conda-build pip ${GET_BOA:-} -c conda-forge
