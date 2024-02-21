8ea96b2a1d386784fc25a79fc801d48dbf5f4bfe
Uwe L. Korn <uwe.korn@quantco.com>
2024-02-21 15:41:24 +0100

    Propagate protobuf version to Python requirements

---
+++ recipe/bld.bat
@@ -0,0 +1,10 @@
+@echo on
+
+set "PROTOBUF_INCLUDE_DIR=%LIBRARY_INC%"
+set "PROTOBUF_LIB_DIR=%LIBRARY_LIB%"
+set "PROTOC=%LIBRARY_BIN%/protoc.exe"
+set "MYSQL_CAPI=%LIBRARY_PREFIX%"
+set "EXTRA_COMPILE_ARGS=/DPROTOBUF_USE_DLLS"
+set "SKIP_VENDOR=1"
+
+%PYTHON% -m pip install . --no-deps --verbose
